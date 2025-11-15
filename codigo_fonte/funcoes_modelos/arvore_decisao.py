4ec9a4dd83072e6d539dd3257183c3ed85ff49f1
JulioCesra <Juliocesar2006n@hotmail.com>
2025-11-15 13:51:23 -0300

    Adiciona novas tabelas e gráficos

--- codigo_fonte/funcoes_modelos/arvore_decisao.py
+++ codigo_fonte/funcoes_modelos/arvore_decisao.py
@@ -1,10 +1,20 @@
 from funcoes_auxiliares.limpar_dataframe import dataframeLimpo
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 df = dataframeLimpo()
 
+def visualizar_dataframe(quantidadeLinhas:int):
+    return df.head(quantidadeLinhas)
+
+def colunas_correlacao():
+    correlacao = df.corr()
+    return correlacao
+
 def grafico_correlacao():
-    fig, axs = plt.subplots()
+    fig, axs = plt.subplots(
+        figsize=(21, 12)
+    )
     correlacao = df.corr()
     sns.heatmap(
         correlacao,
