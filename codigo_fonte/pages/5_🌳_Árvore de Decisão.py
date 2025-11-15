323b829f6087390a62e90424caa894a498203274
JulioCesra <Juliocesar2006n@hotmail.com>
2025-11-15 11:09:25 -0300

    update: Adiciona o gráfico de correlação na página da árvore de decisão

--- codigo_fonte/pages/5_🌳_Árvore de Decisão.py
+++ codigo_fonte/pages/5_🌳_Árvore de Decisão.py
@@ -1,6 +1,16 @@
 import streamlit as st
+from ..funcoes_modelos import arvore_decisao
+
+modelo = arvore_decisao
 
 st.set_page_config(
     page_title='Árvore de Decisão',
     page_icon='🌳' 
+)
+
+sessao_grafico_correlacao = st.container(
+    border=True
+)
+sessao_grafico_correlacao.pyplot(
+    fig = modelo.grafico_correlacao()
 )
